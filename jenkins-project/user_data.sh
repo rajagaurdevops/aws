9db22c467864e22722f8bb0ec4bddf706a917a13
rajagaurdevops <your.rajagaur333@gmail.com>
2025-04-01 12:09:31 +0530

    update jenkins-project:01

--- jenkins-project/user_data.sh
+++ jenkins-project/user_data.sh
@@ -12,8 +12,8 @@ curl -fsSL https://nginx.org/keys/nginx_signing.key | gpg --dearmor -o /usr/shar
 echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | tee /etc/apt/sources.list.d/nginx.list
 
 # Jenkins ke liye repo aur key add karein
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | gpg --dearmor -o /usr/share/keyrings/jenkins-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/jenkins-archive-keyring.gpg] https://pkg.jenkins.io/debian/ binary/" | tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/" | tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 # Update aur latest Nginx aur Jenkins install karein
 apt update -y
